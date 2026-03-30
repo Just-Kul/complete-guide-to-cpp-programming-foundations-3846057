b6cfa69ee9f76e0bcbb10d1a45ac6285863b0685
Just-Kul <justyna.kuleta@gmail.com>
2026-03-30 12:48:04 +0000

    Hello World, by me

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 int main (){
-    std::cout << "Hi, There!" << std::endl;
+    std::cout << "Hi There!" << std::endl;
+    std::cout <<  std::endl <<  std::endl;
     return 0;
 }
