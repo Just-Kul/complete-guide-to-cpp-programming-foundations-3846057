e5b42534c884e562c66329ce93000df1b3c4cd97
Just-Kul <justyna.kuleta@gmail.com>
2026-04-02 08:31:46 +0000

    Vectors, by me

--- src/Ch04/04_06b/CodeDemo.cpp
+++ src/Ch04/04_06b/CodeDemo.cpp
@@ -2,11 +2,24 @@
 // Exercise 04_06
 // The Vector Class, by Eduardo Corpeño 
 
+#include <vector>
 #include <iostream>
 #include <string>
 
 int main(){
+
+    std::vector<std::string> checkpointers = {"checkpointer_1", "checkpointer_2", "checkpointer_3"};
     
+
+    std::cout << "Size of the vector:: " << checkpointers.size()<< std::endl;
+    checkpointers.push_back("checkpointer_4");
+    checkpointers.push_back("checkpointer_5");
+   
+    std::cout << "Size of the vector:: " << checkpointers.size()<< std::endl;
+    checkpointers[0] = "my changed checkpointer";
+
+  std::cout << "Vector at position 0 :: " <<  checkpointers[0] << "." << std::endl;
+   
     std::cout << std::endl << std::endl;
     return 0;
 }
